b463ee62e6bce2ee101588d9b3167ae2fba6dfaf
M4YLANA <pixarshop21@gmail.com>
2023-11-14 14:42:38 +0700

    custom componen

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -15,22 +15,33 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Persegi"),
-          ),
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Segitiga"),
-          )
+          CustomMenu(imageAssets: "assets/img_1.png",title: "Persegi"),
+          CustomMenu(imageAssets: "assets/img.png",title: "Segitiga"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+          color: Colors.green
+      ),
+      child: Column(
+        children: [
+          Image.asset(imageAssets,height: 70,),
+          Text(title),
         ],
       ),
     );
